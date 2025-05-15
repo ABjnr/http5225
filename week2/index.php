c7b33b0d73f37b75a904a7c95d01090c1dda1ed7
Abraham Adeniyi <adeniyiabraham74@gmail.com>
2025-05-14 20:00:33 -0400

    intro to php

---
+++ week2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Hello!</h1>
+    <?php
+        $fname = "Jeffery";
+     $lname = "Berner";
+
+    echo "<h1>Hello Hello</h1>";
+
+    echo "<br><br>";
+
+    echo "Hello " . $fname . " " . $lname;
+
+    echo "<br><br>";
+
+    echo "Hello, wecome to Gary\"s world!";
+
+    ?>
+</body>
+</html>
